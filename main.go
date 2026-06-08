358a28a566b7908eb89448192b182054e6cd8f74
David Clark <me@nobody.com>
2026-06-07 23:35:43 -0500

    Making day func storage to be arrays and not maps to preserve order

--- main.go
+++ main.go
@@ -10,31 +10,49 @@ import (
 	"aoc-2020/day07"
 	"aoc-2020/day08"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
 type DayFunc func() int64
 
-var all map[string]map[string]DayFunc = map[string]map[string]DayFunc{
-	"1": {"1": day01.Part1, "2": day01.Part2},
-	"2": {"1": day02.Part1, "2": day02.Part2},
-	"3": {"1": day03.Part1, "2": day03.Part2},
-	"4": {"1": day04.Part1, "2": day04.Part2},
-	"5": {"1": day05.Part1, "2": day05.Part2},
-	"6": {"1": day06.Part1, "2": day06.Part2},
-	"7": {"1": day07.Part1, "2": day07.Part2},
-	"8": {"1": day08.Part1, "2": day08.Part2},
+var all [][]DayFunc = [][]DayFunc{
+	{day01.Part1, day01.Part2},
+	{day02.Part1, day02.Part2},
+	{day03.Part1, day03.Part2},
+	{day04.Part1, day04.Part2},
+	{day05.Part1, day05.Part2},
+	{day06.Part1, day06.Part2},
+	{day07.Part1, day07.Part2},
+	{day08.Part1, day08.Part2},
 }
 
 func main() {
 	if len(os.Args) == 1 {
-		for day, functions := range all {
-			fmt.Printf("Day %s 1: %d, 2: %d\n", day, functions["1"](), functions["2"]())
+		for idx, functions := range all {
+			day := idx + 1
+			fmt.Printf("Day %d 1: %d, 2: %d\n", day, functions[0](), functions[1]())
 		}
 	} else if len(os.Args) == 2 {
-		functions := all[os.Args[1]]
-		fmt.Printf("Day %s 1: %d, 2: %d\n", os.Args[1], functions["1"](), functions["2"]())
+		day, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalf("could not convert %s to number", os.Args[1])
+		}
+
+		functions := all[day-1]
+		fmt.Printf("Day %d 1: %d, 2: %d\n", day, functions[0](), functions[1]())
 	} else if len(os.Args) == 3 {
-		fmt.Printf("Day %s %s: %d\n", os.Args[1], os.Args[2], all[os.Args[1]][os.Args[2]]())
+		day, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalf("could not convert %s to number", os.Args[1])
+		}
+
+		part, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatalf("could not convert %s to number", os.Args[2])
+		}
+
+		fmt.Printf("Day %d %d: %d\n", day, part, all[day-1][part-1]())
 	}
 }
