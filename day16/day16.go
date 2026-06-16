6ae8dd5ca23cdb3d03106e25c86cc97cf3817ab0
David Clark <me@nobody.com>
2026-06-15 23:44:14 -0500

    Finished part 2 day 16

--- day16/day16.go
+++ day16/day16.go
@@ -4,20 +4,21 @@ import (
 	"aoc-2020/utils"
 	//"fmt"
 	"github.com/IBM/fp-go/array"
+	"maps"
 	"regexp"
 	"strings"
 )
 
-type interval struct {
-	low, high int64
+type intervals struct {
+	low1, high1, low2, high2 int64
 }
 
-func (i interval) inRange(val int64) bool {
-	return i.low <= val && val <= i.high
+func (i intervals) inRange(val int64) bool {
+	return (i.low1 <= val && val <= i.high1) || (i.low2 <= val && val <= i.high2)
 }
 
-func parse() (map[string][]interval, []int64, [][]int64) {
-	fields := map[string][]interval{}
+func parse() (map[string]intervals, []int64, [][]int64) {
+	fields := map[string]intervals{}
 	var your []int64
 	nearby := [][]int64{}
 
@@ -35,8 +36,8 @@ func parse() (map[string][]interval, []int64, [][]int64) {
 		switch mode {
 		case 1: //ranges
 			matches := findRanges.FindStringSubmatch(line)
-			fields[matches[1]] = []interval{interval{utils.ToInt64(matches[2]), utils.ToInt64(matches[3])},
-				interval{utils.ToInt64(matches[4]), utils.ToInt64(matches[5])}}
+			fields[matches[1]] = intervals{utils.ToInt64(matches[2]), utils.ToInt64(matches[3]),
+				utils.ToInt64(matches[4]), utils.ToInt64(matches[5])}
 		case 2: //your ticket
 			your = array.Map(utils.ToInt64)(strings.Split(line, ","))
 		case 3: //nearby tickets
@@ -47,19 +48,17 @@ func parse() (map[string][]interval, []int64, [][]int64) {
 	return fields, your, nearby
 }
 
-func validInSomeRange(fields map[string][]interval, val int64) bool {
-	for _, intervals := range fields {
-		for _, interval := range intervals {
-			if interval.inRange(val) {
-				return true
-			}
+func validInSomeRange(fields map[string]intervals, val int64) bool {
+	for _, toTest := range fields {
+		if toTest.inRange(val) {
+			return true
 		}
 	}
 
 	return false
 }
 
-func invalidSum(fields map[string][]interval, nearby [][]int64) int64 {
+func invalidSum(fields map[string]intervals, nearby [][]int64) int64 {
 	var ret int64
 	for _, testArray := range nearby {
 		for _, toTest := range testArray {
@@ -72,11 +71,70 @@ func invalidSum(fields map[string][]interval, nearby [][]int64) int64 {
 	return ret
 }
 
+func valids(fields map[string]intervals, nearby [][]int64) [][]int64 {
+	ret := [][]int64{}
+outerLoop:
+	for _, testArray := range nearby {
+		for _, toTest := range testArray {
+			if !validInSomeRange(fields, toTest) {
+				continue outerLoop
+			}
+		}
+
+		ret = append(ret, testArray)
+	}
+
+	return ret
+}
+
+func validPosition(toTest intervals, position int, tickets [][]int64) bool {
+	for _, ticket := range tickets {
+		if !toTest.inRange(ticket[position]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func satisfy(fields map[string]intervals, positions []string, tickets [][]int64) (bool, []string) {
+	if len(positions) == len(tickets[0]) {
+		return true, positions
+	}
+
+	for field, toTest := range fields {
+		if validPosition(toTest, len(positions), tickets) {
+			newFields := maps.Clone(fields)
+			delete(newFields, field)
+			newPositions := append(positions, field)
+			isValid, newPositions := satisfy(newFields, newPositions, tickets)
+			if isValid {
+				return isValid, newPositions
+			}
+		}
+	}
+
+	return false, []string{} //keep compiler happy, should not get here
+}
+
 func Part1() int64 {
 	fields, _, nearby := parse()
 	return utils.TestResult(16, 1, 23036, invalidSum(fields, nearby))
 }
 
 func Part2() int64 {
-	return utils.TestResult(16, 2, 0, 0)
+	fields, yours, nearby := parse()
+	allValid := valids(fields, nearby)
+	isValid, positions := satisfy(fields, []string{}, allValid)
+
+	var result int64 = int64(1)
+	if isValid {
+		for idx, name := range positions {
+			if strings.HasPrefix(name, "departure") {
+				result *= yours[idx]
+			}
+		}
+	}
+
+	return utils.TestResult(16, 2, 1909224687553, result)
 }
